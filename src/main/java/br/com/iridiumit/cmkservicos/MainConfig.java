c985a57195145f4c74001961d98347886f4083e7
aatan <aatan@LAPTOP-I2Q04JVD>
2020-05-03 18:12:02 -0300

    Inclusão de páginas de erro customizadas
    Configuração inicial na máquina de desenvolvimento nova

--- src/main/java/br/com/iridiumit/cmkservicos/MainConfig.java
+++ src/main/java/br/com/iridiumit/cmkservicos/MainConfig.java
@@ -8,7 +8,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
-@Configuration
+//@Configuration
 @ComponentScan(basePackages = "br.com.iridiumit.cmkservicos")
 public class MainConfig {
 
